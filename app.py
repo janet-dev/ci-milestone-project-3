15a0eae5c5bebc94335f349e346f0c2c6cbcfe8a
JD <janet_dornan@hotmail.com>
2023-03-23 18:32:48 +0000

    Add 404 page

--- app.py
+++ app.py
@@ -20,6 +20,12 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 def login_required_admin(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
