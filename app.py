ef2c03e7c06098368ca8266f7353b19be5090144
JD <janet_dornan@hotmail.com>
2023-04-06 16:42:01 +0000

    Add os environ variable for debug

--- app.py
+++ app.py
@@ -381,4 +381,4 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=False)
+        debug=os.environ.get("DEBUG"))
