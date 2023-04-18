7fc59a35a52caf280587f6b7115a2bd9357e5000
JD <janet_dornan@hotmail.com>
2023-04-18 16:27:23 +0000

    Remove visibility:hidden to jQuery for select.validate

--- static/js/script.js
+++ static/js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
         let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
         if ($("select.validate").prop("required")) {
-            $("select.validate").css({ "visibility": "hidden", "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
         }
         $(".select-wrapper input.select-dropdown").on("focusin", function () {
             $(this).parent(".select-wrapper").on("change", function () {
