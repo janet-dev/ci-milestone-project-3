9f285ffa19fdb939564d14448a49027c8d0ab08a
JD <janet_dornan@hotmail.com>
2023-04-19 15:32:06 +0000

    Remove extra 2 formSelect in jquery code

--- static/js/script.js
+++ static/js/script.js
@@ -6,8 +6,6 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".tooltipped").tooltip();
     $("select").formSelect();
-    $("select").formSelect();
-    $("select").formSelect();
 
     /* Code from Tim Nelson (CI) for adding custom validation to Materialize dropdown */
     validateMaterializeSelect();
